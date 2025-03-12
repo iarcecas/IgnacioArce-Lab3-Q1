0ddb26e6680bab24ed60d908a0fc9692778b50c3
Ignacio Arce <iarcecas@my.centennialcollege.ca>
2025-03-12 10:59:21 -0400

    Updated Application

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:21
+
+ADD target/spring-devops.jar spring-devops.jar
+
+ENTRYPOINT ["java", "-jar", "/spring-devops.jar"]
--- src/main/java/com/example/demo/IgnacioArceLab3Q1Application.java
+++ src/main/java/com/example/demo/IgnacioArceLab3Q1Application.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import java.time.LocalTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class IgnacioArceLab3Q1Application {
 
 	public static void main(String[] args) {
+
+		
 		SpringApplication.run(IgnacioArceLab3Q1Application.class, args);
+	}
+	
+	@GetMapping("/get")
+	public String getMessage() {
+        LocalTime currentTime = LocalTime.now();
+        String message;
+        if (currentTime.isAfter(LocalTime.of(5, 0)) && currentTime.isBefore(LocalTime.of(12, 0))) {
+            message = "Good morning Ignacio! Welcome to COMP367";
+        } else if (currentTime.isAfter(LocalTime.of(12, 0)) && currentTime.isBefore(LocalTime.of(17, 0))) {
+            message = "Good afternoon Ignacio! Welcome to COMP367";
+        } else {
+            message = "Good evening Ignacio! Welcome to COMP367";
+        }
+        
+        return message;
 	}
 
 }
